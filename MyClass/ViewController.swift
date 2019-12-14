246b400273396f854b4b77386c64b1d802839a12
naohisa0616 <naohisa0616@icloud.com>
2019-12-14 16:28:19 +0900

    Computedクラスプロパティ ・radianクラスプロパティとComputedのdegreeクラスプロパティがあるMyClassクラス ・degreeクラスプロパティを18度に設定する

--- MyClass/ViewController.swift
+++ MyClass/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
+class MyClass {
+//    クラスプロパティ
+    static var radian:Double = 0.0
+    
+//    Computed クラスプロパティ degree
+    static var degree:Double {
+//        値の取り出し
+        get {
+//            radianをディグリに変換して返す
+            let dosu = radian * 90/Double.pi
+            return dosu
+        }
+        
+//        値の設定
+        set (dosu) {
+//          設定値をラジアンに変換してradianに設定する
+            radian = dosu * Double.pi/90
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //傾きを18度に設定
+//        値の設定なのでsetを呼び出します
+        MyClass.degree = 18
+//        値を取り出すのでgetを呼び出します
+        let katamuki = MyClass.degree
+        print("\(katamuki)度")
+        
     }
 
 
